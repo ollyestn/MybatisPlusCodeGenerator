16d6d9a36ac9cebbfe71bae3f155ffc21e5765cb
ollyestn <ollyestn@126.com>
2025-08-03 17:35:16 +0800

    修改test文件

---
+++ src/test/java/com/xyzy/fusion/mybatispluscodegenerator/MybatisPlusCodeGeneratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xyzy.fusion.mybatispluscodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
